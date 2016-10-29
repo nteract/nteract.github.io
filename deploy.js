b37a54959b87817d13d2e706d7c6203962b4a6e1
Lukas Geiger <lukas.geiger94@gmail.com>
2016-10-29 22:43:22 +0200

    Travis: Add user information to deploy script

--- deploy.js
+++ deploy.js
@@ -3,5 +3,10 @@ var path = require('path');
 
 ghpages.publish(path.join(__dirname, 'dist'), {
   branch: 'master',
-  repo: 'https://${GH_TOKEN}@github.com/nteract/nteract.github.io.git'
+  repo: 'https://${GH_TOKEN}@github.com/nteract/nteract.github.io.git',
+  silent: true,
+  user: {
+    name: 'Travis Bot',
+    email: 'travis@travis-ci.com'
+  }
 });
