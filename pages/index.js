b817f96d6426a8aa7b83ed7d5fa5f7d07f5237d7
Kyle Kelley <rgbkrk@gmail.com>
2017-01-31 18:52:32 -0800

    Now for most of the main page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Footer from '../components/footer'
 import Head from '../components/head'
 import Header from '../components/header'
+import Main from '../components/main'
 
 export default () => (
   <div>
     <Head />
     <Header />
+    <Main />
     <Footer />
   </div>
 )
