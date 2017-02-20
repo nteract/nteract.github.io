d39646bd9e57dcdfb54f46eb977f49ae83f0f78b
Kyle Kelley <rgbkrk@gmail.com>
2017-02-19 16:40:48 -0800

    Force jquery onto the page.

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Head from '../components/head'
 import Header from '../components/header'
 import Main from '../components/main'
 
+const $ = require('jquery');
+global.$ = $;
+global.jQuery = $;
+
 export default () => (
   <div>
     <Head />
